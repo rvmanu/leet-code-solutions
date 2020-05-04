b5eb0ed6489c5b9ab060d720b36d76162524a3aa
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-04 22:37:16 +0530

    First Bad Version - Solution

---
+++ Leet Code Solutions/FirstBadVersion/FirstBadVersion/FirstBadVersion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.FirstBadVersion
+{
+    class FirstBadVersion
+    {
+        public int GetFirstBadVersion(int n)
+        {
+            var low = 0;
+            var high = n;
+            while (low < high)
+            {
+                // Calculating midpoint using (low + high)/2 vs low + ((high-low) / 2) : https://stackoverflow.com/questions/21101110/calculating-midpoint-index-in-binary-search
+                // var midPoint = (low + high) / 2;
+
+                // Best practice is to use this for finding mid-point
+                var midPoint = low + Convert.ToInt32(Math.Floor(Convert.ToDouble((high - low) / 2)));
+                var isMidPointBad = IsBadVersion(midPoint);
+                if (isMidPointBad)
+                {
+                    high = midPoint;
+                }
+                else
+                {
+                    low = midPoint + 1;
+                }
+            }
+
+            return low;
+        }
+
+        private static bool IsBadVersion(int midPoint)
+        {
+            return true;
+        }
+    }
+}
---
+++ Leet Code Solutions/FirstBadVersion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+}
