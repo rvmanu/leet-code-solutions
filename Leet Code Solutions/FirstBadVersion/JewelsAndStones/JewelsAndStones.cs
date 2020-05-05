013d6945185fb0dce709dc0a9d70c62e10f558f5
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-05 20:45:53 +0530

    Added solutions for problems: JewelsAndStones and RansomNote

---
+++ Leet Code Solutions/FirstBadVersion/JewelsAndStones/JewelsAndStones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.JewelsAndStones
+{
+    class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            //Lookup is more hence use a dictionary to reduce the lookup time to O(1)
+            var charMap = new Dictionary<char, int>();
+            for (var i = 0; i < J.Length; i++)
+            {
+                charMap.Add(J[i], 0);
+            }
+
+            var count = 0;
+            for (var j = 0; j < S.Length; j++)
+            {
+                //Dictionary is not the ideal DS for this purpose since we are not fetching the value.
+                //But this is more performant than using nested loops
+                if (charMap.ContainsKey(S[j]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Leet Code Solutions/FirstBadVersion/RansomNote/RansomNote.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.RansomNote
+{
+    class RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+                return false;
+            else if (ransomNote.Length == 0 || ransomNote == magazine)
+            {
+                return true;
+            }
+            else if (ransomNote.Length > 0)
+            {
+                var charMap = new int[26];
+                for (var i = 0; i < magazine.Length; i++)
+                {
+                    charMap[magazine[i] - 'a']++;
+                }
+
+                for (var i = 0; i < ransomNote.Length; i++)
+                {
+                    if (charMap[ransomNote[i] - 'a'] > 0)
+                    {
+                        charMap[ransomNote[i] - 'a']--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool CanConstructRansomNote(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+                return false;
+            else if (ransomNote.Length == 0)
+            {
+                return true;
+            }
+            else if (ransomNote.Length > 0)
+            {
+                var charMap = new Dictionary<char, int>();
+                for (var i = 0; i < magazine.Length; i++)
+                {
+                    if (charMap.ContainsKey(magazine[i]))
+                    {
+                        charMap[magazine[i]]++;
+                    }
+                    else
+                    {
+                        charMap.Add(magazine[i], 1);
+                    }
+                }
+
+                for (var i = 0; i < ransomNote.Length; i++)
+                {
+                    if (charMap.ContainsKey(ransomNote[i]) && charMap[ransomNote[i]] > 0)
+                    {
+                        charMap[ransomNote[i]]--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
