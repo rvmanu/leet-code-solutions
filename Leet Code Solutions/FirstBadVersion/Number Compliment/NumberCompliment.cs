0720a50191ef0bfeaf06f7a2f7d740b09f2cb207
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-06 10:31:18 +0530

    Added solution for Number Compliment problem

---
+++ Leet Code Solutions/FirstBadVersion/Number Compliment/NumberCompliment.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Number_Compliment
+{
+    class NumberCompliment
+    {
+        public int FindComplement1(int num)
+        {
+            //num XOR 1s = comp
+            var result = num;
+            var digit = (int)Math.Log(num, 2) + 1;
+            for (var i = 0; i < digit; i++)
+            {
+                result = result ^ (1 << i);
+            }
+
+            return result;
+        }
+
+        public int FindComplement2(int num)
+        {
+            //num XOR 1s = comp
+            var result = num;
+            var digit = 0;
+            while (num > 0)
+            {
+                result = result ^ (1 << digit);
+                num = num / 2;
+                digit++;
+            }
+
+            return result;
+        }
+
+        public int FindComplement3(int num)
+        {
+            //num XOR 1s = comp
+            return num ^ (int)Math.Pow(2, (int)Math.Log(num, 2) + 1) - 1;
+        }
+
+        public int FindComplement4(int num)
+        {
+            var result = 0;
+            var stack = new Stack<int>();
+            while (num != 0)
+            {
+                var digit = num % 2;
+                stack.Push(digit == 0 ? 1 : 0);
+                num = num / 2;
+            }
+
+            while (stack.Count > 0)
+            {
+                var digit = stack.Pop();
+                result = result * 2 + digit;
+            }
+
+            return result;
+        }
+    }
+}
