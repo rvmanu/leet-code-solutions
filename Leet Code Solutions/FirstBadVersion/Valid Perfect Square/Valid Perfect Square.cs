37b3738148218d1f7bc9cf772d3cfdd89154be38
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-09 22:19:53 +0530

    Solution - Valid Perfect Square

---
+++ Leet Code Solutions/FirstBadVersion/Valid Perfect Square/Valid Perfect Square.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Valid_Perfect_Square
+{
+    class Valid_Perfect_Square
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long result = 0;
+            long number = num;
+            for (long i = 1; (i * i) <= number; i++)
+            {
+                result = i * i;
+            }
+
+            return result == number;
+        }
+
+        public bool IsPerfectSquare(int num)
+        {
+            if (num == 0 || num == 1) return true;
+
+            var low = 2;
+            var high = num;
+
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                if (mid == num / mid)
+                {
+                    return num % mid == 0;
+                }
+                else if (mid > num / mid)
+                {
+                    high = mid - 1;
+                }
+                else if (mid < num / mid)
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
