e738d87723380f988afa844ab656c2ba90dfb6ba
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-19 10:37:31 +0530

    Solution - Best Time to Buy And Sell Stock - II

---
+++ Leet Code Solutions/FirstBadVersion/Best Time to Buy and Sell Stock II/Best Time To Buy And Sell Stock II.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.NewFolder
+{
+    class Best_Time_To_Buy_And_Sell_Stock_II
+    {
+        public int MaxProfit(int[] prices)
+        {
+            // one day eithor buy/sell/skip transaction on any no of units and price during that day remains constant.
+            // We get profit when buy at low and sell at high
+            // One will get max profit wheh all such instances are utilized
+            var profit = 0;
+            for (var i = 0; i < prices.Length - 1; i++)
+            {
+                // buy at low and sell at high
+                if (prices[i] < prices[i + 1])
+                    profit += (prices[i + 1] - prices[i]);
+            }
+
+            return profit;
+        }
+    }
+}
