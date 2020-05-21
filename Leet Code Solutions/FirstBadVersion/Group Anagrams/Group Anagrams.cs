af21b3ea59eda83335be718de147f617510e8aeb
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-05-22 00:23:59 +0530

    Solution - Group Anagrams, Middle of Linked List

---
+++ Leet Code Solutions/FirstBadVersion/Group Anagrams/Group Anagrams.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Group_Anagrams
+{
+    class Group_Anagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new List<IList<string>>();
+            if (strs.Length == 0)
+            {
+                result.Add(new List<string>());
+                return result as IList<IList<string>>;
+            }
+            else if (strs.Length == 1)
+            {
+                result.Add(new List<string>() { strs[0] });
+                return result as IList<IList<string>>;
+            }
+
+            result.Add(new List<string>() { strs[0] });
+
+            for (var i = 1; i < strs.Length; i++)
+            {
+                var k = 0;
+                var isFound = false;
+                while (k < result.Count)
+                {
+                    var areSame = isAnagram(result[k][0], strs[i]);
+                    if (areSame)
+                    {
+                        isFound = true;
+                        result[k].Add(strs[i]);
+                        break;
+                    }
+
+                    k++;
+                }
+
+                if (!isFound)
+                {
+                    result.Add(new List<string>() { strs[i] });
+                }
+            }
+
+            return result as IList<IList<string>>;
+        }
+
+        private bool isAnagram(string x, string y)
+        {
+            if (x.Length != y.Length)
+                return false;
+            else
+            {
+                var charMap = new int[26];
+                foreach (var c in y)
+                {
+                    charMap[c - 'a']++;
+                }
+
+                foreach (var c in x)
+                {
+                    charMap[c - 'a']--;
+                }
+
+                foreach (var c in y)
+                {
+                    if (charMap[c - 'a'] != 0)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Leet Code Solutions/FirstBadVersion/Middle of Linked List/Middle of Linked List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Middle_of_Linked_List
+{
+    class Middle_of_Linked_List
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            else if (head.next.next == null)
+                return head.next;
+
+            var node = head;
+            var runner = head;
+            while (runner != null && runner.next != null)
+            {
+                node = node.next;
+                runner = runner.next.next;
+            }
+
+            return node;
+        }
+    }
+}
