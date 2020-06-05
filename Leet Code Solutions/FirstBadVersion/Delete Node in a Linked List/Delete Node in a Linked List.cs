51e9cff025b082c6bf0418b2ac2ca72368f8deae
Manomohanan Rv <Manomohanan.Rv@neudesic.com>
2020-06-05 08:52:42 +0530

    Soltions - Reverse String, Delete Node in a Linked List

---
+++ Leet Code Solutions/FirstBadVersion/Delete Node in a Linked List/Delete Node in a Linked List.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Delete_Node_in_a_Linked_List
+{
+    class Delete_Node_in_a_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public class Solution
+        {
+            public void DeleteNode(ListNode node)
+            {
+                if (node.next != null)
+                {
+                    node.val = node.next.val;
+                    node.next = node.next.next;
+                }
+
+
+                /*
+                Complexity: O(N)
+                var runner = node;
+                while(node.next != null)
+                {
+                    node.val = node.next.val;
+                    runner = node;
+                    node = node.next;
+                }
+
+                runner.next = null;*/
+            }
+        }
+    }
+}
---
+++ Leet Code Solutions/FirstBadVersion/Reverse a String/Reverse s String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBadVersion.Reverse_a_String
+{
+    class Reverse_s_String
+    {
+        public void ReverseString(char[] s)
+        {
+            for (var i = 0; i < s.Length / 2; i++)
+            {
+                var temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = temp;
+            }
+        }
+    }
+}
